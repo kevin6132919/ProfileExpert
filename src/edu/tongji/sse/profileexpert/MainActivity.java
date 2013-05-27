933e61bca5559ee99478b16c452c3ffd165dfcdc
ZhongLing <kevin6132919@163.com>
2013-05-27 18:27:58 +0800

    全屏显示
    Signed-off-by: ZhongLing <kevin6132919@163.com>

--- src/edu/tongji/sse/profileexpert/MainActivity.java
+++ src/edu/tongji/sse/profileexpert/MainActivity.java
@@ -1,14 +1,18 @@
 package edu.tongji.sse.profileexpert;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //…Ë÷√»´∆¡œ‘ æ
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
