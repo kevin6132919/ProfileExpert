80ab7db8fe05d1705371e098fecfe33a062b79b3
ZhongLing <kevin6132919@163.com>
2013-06-05 22:33:31 +0800

    新建临时事项界面设计

--- src/edu/tongji/sse/profileexpert/control/MyDateSpinner.java
+++ src/edu/tongji/sse/profileexpert/control/MyDateSpinner.java
@@ -1,5 +1,6 @@
 package edu.tongji.sse.profileexpert.control;
 
+import edu.tongji.sse.profileexpert.R;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Context;
 import android.graphics.Color;
@@ -12,13 +13,14 @@ import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
  
-public class MyDateSpinner extends Spinner {
-    public MyDateSpinner(Context context) {
+public class MyDateSpinner extends Spinner
+{
+    public MyDateSpinner(Context context){
         super(context);
-        // TODO Auto-generated constructor stub
     }
  
-    public MyDateSpinner(Context context, AttributeSet attrs) {
+    public MyDateSpinner(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
         if (isInEditMode()) {
             return;
@@ -26,36 +28,34 @@ public class MyDateSpinner extends Spinner {
         final Time time = new Time();
         time.setToNow();
         //为MyDateSpinner设置adapter，主要用于显示spinner的text值
-        MyDateSpinner.this.setAdapter(new BaseAdapter() {
- 
+        MyDateSpinner.this.setAdapter(new BaseAdapter()
+        {
             @Override
             public int getCount() {
-                // TODO Auto-generated method stub
                 return 1;
             }
  
             @Override
             public Object getItem(int arg0) {
-                // TODO Auto-generated method stub
                 return null;
             }
  
             @Override
             public long getItemId(int arg0) {
-                // TODO Auto-generated method stub
                 return 0;
             }
  
             @Override
-            public View getView(int arg0, View arg1, ViewGroup arg2) {
-                // TODO Auto-generated method stub
+            public View getView(int position, View convertView, ViewGroup parent)
+            {
                 TextView text = new TextView(MyDateSpinner.this.getContext());
+                text.setTextSize(getResources().getDimension(R.dimen.time_text_size));
                 text.setText(time.year
                         + "-"
                         + (time.month + 1)
                         + "-"
                         + time.monthDay
-                        + MyDatePickerDialog.CaculateWeekDay(time.year,
+                        + MyDatePickerDialog.caculateWeekDay(time.year,
                                 time.month+1, time.monthDay));
                 text.setTextColor(Color.BLACK);
                 return text;
@@ -64,49 +64,47 @@ public class MyDateSpinner extends Spinner {
     }
  
     @Override
-    public boolean performClick() {
+    public boolean performClick()
+    {
         Time time = new Time();
         time.setToNow();
         MyDatePickerDialog tpd = new MyDatePickerDialog(getContext(),
-                new OnDateSetListener() {
- 
+                new OnDateSetListener()
+        		{
                     @Override
                     public void onDateSet(DatePicker view, final int year,
-                            final int month, final int day) {
-                        // TODO Auto-generated method stub
+                            final int month, final int day)
+                    {
                         //为MyDateSpinner动态设置adapter，主要用于修改spinner的text值
                         MyDateSpinner.this.setAdapter(new BaseAdapter() {
  
                             @Override
                             public int getCount() {
-                                // TODO Auto-generated method stub
                                 return 1;
                             }
  
                             @Override
                             public Object getItem(int arg0) {
-                                // TODO Auto-generated method stub
                                 return null;
                             }
  
                             @Override
                             public long getItemId(int arg0) {
-                                // TODO Auto-generated method stub
                                 return 0;
                             }
  
                             @Override
                             public View getView(int arg0, View arg1,
                                     ViewGroup arg2) {
-                                // TODO Auto-generated method stub
                                 TextView text = new TextView(MyDateSpinner.this
                                         .getContext());
+                                text.setTextSize(getResources().getDimension(R.dimen.time_text_size));
                                 text.setText(year
                                         + "-"
                                         + (month + 1)
                                         + "-"
                                         + day
-                                        + MyDatePickerDialog.CaculateWeekDay(
+                                        + MyDatePickerDialog.caculateWeekDay(
                                                 year, month + 1, day));
                                 text.setTextColor(Color.BLACK);
                                 return text;
---
+++ src/edu/tongji/sse/profileexpert/control/MyTimeSpinner.java
@@ -0,0 +1,124 @@
+package edu.tongji.sse.profileexpert.control;
+
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.format.Time;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import edu.tongji.sse.profileexpert.R;
+ 
+public class MyTimeSpinner extends Spinner
+{
+    public MyTimeSpinner(Context context){
+        super(context);
+    }
+ 
+    public MyTimeSpinner(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        if (isInEditMode()) {
+            return;
+        }
+        final Time time = new Time();
+        time.setToNow();
+        //为MyDateSpinner设置adapter，主要用于显示spinner的text值
+        MyTimeSpinner.this.setAdapter(new BaseAdapter()
+        {
+            @Override
+            public int getCount() {
+                return 1;
+            }
+ 
+            @Override
+            public Object getItem(int arg0) {
+                return null;
+            }
+ 
+            @Override
+            public long getItemId(int arg0) {
+                return 0;
+            }
+ 
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+            {
+                TextView text = new TextView(MyTimeSpinner.this.getContext());
+                text.setTextSize(getResources().getDimension(R.dimen.time_text_size));
+                text.setText(formatHourAndMinute(time.hour,time.minute));
+                text.setTextColor(Color.BLACK);
+                return text;
+            }
+        });
+    }
+ 
+    @Override
+    public boolean performClick()
+    {
+        Time time = new Time();
+        time.setToNow();
+        TimePickerDialog tpd = new TimePickerDialog(
+        		getContext(),
+        		new OnTimeSetListener()
+        		{
+					@Override
+					public void onTimeSet(TimePicker view, final int hourOfDay, final int minute)
+					{
+						//为MyDateSpinner设置adapter，主要用于显示spinner的text值
+				        MyTimeSpinner.this.setAdapter(new BaseAdapter()
+				        {
+				            @Override
+				            public int getCount() {
+				                return 1;
+				            }
+				 
+				            @Override
+				            public Object getItem(int arg0) {
+				                return null;
+				            }
+				 
+				            @Override
+				            public long getItemId(int arg0) {
+				                return 0;
+				            }
+				 
+				            @Override
+				            public View getView(int position, View convertView, ViewGroup parent)
+				            {
+				                TextView text = new TextView(MyTimeSpinner.this.getContext());
+				                text.setTextSize(getResources().getDimension(R.dimen.time_text_size));
+				                text.setText(formatHourAndMinute(hourOfDay,minute));
+				                text.setTextColor(Color.BLACK);
+				                return text;
+				            }
+				        });
+					}
+				},
+        		time.hour,
+        		time.minute,
+        		true);
+        tpd.show();
+        return true;
+    }
+    
+    public static String formatHourAndMinute(int hourOfDay, int minute)
+    {
+    	String str = "";
+    	if(hourOfDay < 10)
+    		str += "0"+hourOfDay;
+    	else
+    		str += hourOfDay;
+    	str += ":";
+    	if(minute < 10)
+    		str += "0"+minute;
+    	else
+    		str += minute;
+    	return str;
+    }
+}
